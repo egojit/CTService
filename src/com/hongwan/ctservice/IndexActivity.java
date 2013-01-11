a53c865fb91d7b87f9b009ce733bd232cccfc492
Jack Shao <jack.shao@qq.com>
2013-01-11 22:21:44 +0800

    添加了几个功能按钮的单击事件处理。

--- src/com/hongwan/ctservice/IndexActivity.java
+++ src/com/hongwan/ctservice/IndexActivity.java
@@ -2,6 +2,8 @@ package com.hongwan.ctservice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class IndexActivity extends Activity {
 
@@ -10,6 +12,43 @@ public class IndexActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_index);
+        
+        // set button click event handler
+        final ButtonClickHander handlerButtonClick = new ButtonClickHander();
+        findViewById(R.id.imageButtonCLCX).setOnClickListener(handlerButtonClick);
+		findViewById(R.id.imageButtonBXBJ).setOnClickListener(handlerButtonClick);
+		findViewById(R.id.imageButtonCTSC).setOnClickListener(handlerButtonClick);
+		findViewById(R.id.imageButtonJSCX).setOnClickListener(handlerButtonClick);
+		findViewById(R.id.imageButtonJTZX).setOnClickListener(handlerButtonClick);
+		findViewById(R.id.imageButtonTHSJ).setOnClickListener(handlerButtonClick);
+	}
+	
+	private class ButtonClickHander implements OnClickListener {
+
+		@Override
+		public void onClick(View view) {
+			switch (view.getId()) {
+			// 保险报警
+			case R.id.imageButtonBXBJ:
+				break;
+			// 车辆查询
+			case R.id.imageButtonCLCX:
+				break;
+			// 畅通商城
+			case R.id.imageButtonCTSC:
+				break;
+			// 驾驶查询
+			case R.id.imageButtonJSCX:
+				break;
+			// 交通资讯
+			case R.id.imageButtonJTZX:
+				break;
+			// 特惠商家
+			case R.id.imageButtonTHSJ:
+				break;
+			}
+		}
+		
 	}
 
 }
