56b708246cff40b5e0b7e9aa9e76c5cde70de306
gao.lu <408365330@qq.com>
2013-01-17 23:18:51 +0800

    1.添加了车辆查询2的界面文件，2添加了部分页列表的模板

---
+++ src/com/hongwan/ctservice/CarSearchActivity2.java
@@ -0,0 +1,15 @@
+package com.hongwan.ctservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarSearchActivity2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_car_search2);
+	}
+
+}
